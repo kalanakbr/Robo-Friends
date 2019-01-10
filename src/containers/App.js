08af53ea5e5a5e8eaa20c7c0d6cb0665167b703f
Kalana Rathnayake <39268354+kalanakbr@users.noreply.github.com>
2019-01-10 09:45:16 +0530

    Upload project

---
+++ src/containers/App.js
@@ -0,0 +1,69 @@
+import React ,{ Component }from 'react';
+import { connect } from 'react-redux';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import  Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+import { setSearchField , requestRobots} from '../actions.js';
+
+const mapStateToProps = state =>{
+    return {
+        searchField: state.searchRobots.searchField,
+        robots :state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+ const mapDispatchToProps = (dispatch) => {
+     return{
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+     }
+ }
+class App extends Component  {
+    // constructor(){
+    //     super()
+    //     this.state ={
+    //         robots:[] ,
+            
+    //     }
+    // }
+
+componentDidMount(){
+        this.props.onRequestRobots();
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => {this.setState({ robots:users })});   
+}
+
+   
+    render (){
+      //  const { robots } =this.state;
+        const { searchField,onSearchChange,robots,isPending  } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
+        })
+        if(isPending){
+            return <h1> Loading</h1>
+        }
+        else{
+         return (
+            <div className='tc'>
+            <h1 className='f2'>Robo Friends</h1>
+             <SearchBox searchChange={onSearchChange}/>
+            <Scroll>
+                <ErrorBoundry>
+                    <CardList robots = {filteredRobots}/>
+                </ErrorBoundry>
+            </Scroll>
+            </div>
+            );
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
+ 
---
+++ src/reducers.js
@@ -0,0 +1,41 @@
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants.js';   
+
+ const intialStateSearch = {
+    searchField: ''
+}
+
+export const searchRobots = (state=intialState,action= { }) => {
+    switch(action.type){
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({},state,{searchField: action.payload })
+        default:
+            return state;
+        }
+}
+
+const initialStateRobots = {
+    isPending:false,
+    robots:[],
+    error:''
+}
+
+export const requestRobots = ( state=initialStateRobots,action={ }) => {
+    switch(action.type){
+       case REQUEST_ROBOTS_PENDING:
+            return Object.assign({},state,{ isPending: true })
+        case REQUEST_ROBOTS_SUCCESS:
+            return Object.assign({},{ robots:action.payload,isPending:false })
+        case REQUEST_ROBOTS_FAILED:
+            return Ooject.assign({},state,{error:action.payload,isPending:false } )
+        default:
+            return state;
+        }
+}
+
+
+
